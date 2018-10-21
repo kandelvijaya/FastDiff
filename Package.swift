0c59f6e0c9ce77f532d9291beef76cd57df91b98
vkandel <vijaya.prakash.kandel@zalando.de>
2018-10-21 09:58:42 +0200

    Added AlgoChecker dependency for tests

--- Package.swift
+++ Package.swift
@@ -9,13 +9,15 @@ let package = Package(
             name: "FastDiff",
             targets: ["FastDiff"]),
     ],
-    dependencies: [ ],
+    dependencies: [
+        .package(url: "git@github.com:kandelvijaya/AlgorithmChecker.git", from: "0.1.0"),
+    ],
     targets: [
         .target(
             name: "FastDiff",
             dependencies: []),
         .testTarget(
             name: "FastDiffTests",
-            dependencies: ["FastDiff"]),
+            dependencies: ["FastDiff", "AlgoChecker"]),
     ]
 )
